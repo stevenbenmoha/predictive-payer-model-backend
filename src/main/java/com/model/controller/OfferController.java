fb3dd1da70da3286a3fb9c0b9c12cf40c37968d7
Steven Benmoha <stevenbenmoha@stevens-mbp.home>
2019-07-12 19:15:50 -0400

    created basic plot of data with pythin

--- src/main/java/com/model/controller/OfferController.java
+++ src/main/java/com/model/controller/OfferController.java
@@ -5,14 +5,18 @@ import com.model.service.OfferService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import smile.classification.LogisticRegression;
-import smile.data.*;
-import smile.data.parser.ArffParser;
+import smile.data.Attribute;
+import smile.data.AttributeDataset;
+import smile.data.NominalAttribute;
+import smile.data.NumericAttribute;
 import smile.data.parser.DelimitedTextParser;
+import smile.plot.Plot;
+import smile.plot.PlotCanvas;
+import smile.plot.ScatterPlot;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -36,43 +40,52 @@ public class OfferController {
         DelimitedTextParser delimitedTextParser = new DelimitedTextParser();
         Attribute[] attributes = new Attribute[26];
 
-        attributes[0] = new StringAttribute("Channel");
-        attributes[1] = new StringAttribute("Customer");
-        attributes[2] = new StringAttribute("Segment/Plan");
-        attributes[3] = new StringAttribute("Deal_Level_of_Control");
-        attributes[4] = new StringAttribute("Therapeutic_Area");
-        attributes[5] = new StringAttribute("Product");
+        attributes[0] = new NominalAttribute("Channel");
+        attributes[1] = new NominalAttribute("Customer");
+        attributes[2] = new NominalAttribute("Segment/Plan");
+        attributes[3] = new NominalAttribute("Deal_Level_of_Control");
+        attributes[4] = new NominalAttribute("Therapeutic_Area");
+        attributes[5] = new NominalAttribute("Product");
         attributes[6] = new NumericAttribute("WAC_Price");
-        attributes[7] = new StringAttribute("Start_Date");
-        attributes[8] = new StringAttribute("End_Date");
-        attributes[9] = new StringAttribute("2018_Status_Quo_Brand_Manufacturer");
-        attributes[10] = new StringAttribute("2018_Status_Quo_Tier");
-        attributes[11] = new StringAttribute("2018_Status_Quo_Placement");
-        attributes[12] = new StringAttribute("2018_Status_Quo_Restriction");
-        attributes[13] = new StringAttribute("Deal_Brand_Manufacturer");
-        attributes[14] = new StringAttribute("Deal_Tier");
-        attributes[15] = new StringAttribute("Deal_Placement");
-        attributes[16] = new StringAttribute("Deal_Restriction");
+        attributes[7] = new NominalAttribute("Start_Date");
+        attributes[8] = new NominalAttribute("End_Date");
+        attributes[9] = new NominalAttribute("2018_Status_Quo_Brand_Manufacturer");
+        attributes[10] = new NominalAttribute("2018_Status_Quo_Tier");
+        attributes[11] = new NominalAttribute("2018_Status_Quo_Placement");
+        attributes[12] = new NominalAttribute("2018_Status_Quo_Restriction");
+        attributes[13] = new NominalAttribute("Deal_Brand_Manufacturer");
+        attributes[14] = new NominalAttribute("Deal_Tier");
+        attributes[15] = new NominalAttribute("Deal_Placement");
+        attributes[16] = new NominalAttribute("Deal_Restriction");
         attributes[17] = new NumericAttribute("Deal_Rebate");
-        attributes[18] = new StringAttribute("PP_Type");
+        attributes[18] = new NominalAttribute("PP_Type");
         attributes[19] = new NumericAttribute("PP_Threshold");
-        attributes[20] = new StringAttribute("PP_Lookback_Date");
-        attributes[21] = new StringAttribute("2019_Actual_Brand_Manufacturer");
-        attributes[22] = new StringAttribute("2019_Actual_Tier");
-        attributes[23] = new StringAttribute("2019_Actual_Placement");
-        attributes[24] = new StringAttribute("2019_Actual_Restriction");
+        attributes[20] = new NominalAttribute("PP_Lookback_Date");
+        attributes[21] = new NominalAttribute("2019_Actual_Brand_Manufacturer");
+        attributes[22] = new NominalAttribute("2019_Actual_Tier");
+        attributes[23] = new NominalAttribute("2019_Actual_Placement");
+        attributes[24] = new NominalAttribute("2019_Actual_Restriction");
         attributes[25] = new NumericAttribute("Share_Shift");
         Attribute attribute26 = new NominalAttribute("Decision");
 
         delimitedTextParser.setDelimiter(",");
+        delimitedTextParser.setColumnNames(true);
         delimitedTextParser.setResponseIndex(attribute26, 26);
         AttributeDataset deals = delimitedTextParser.parse(attributes, "/Users/stevenbenmoha/IdeaProjects/payer-model/payer-model/src/main/java/com/model/data/data.csv");
         double[][] x = deals.toArray(new double[deals.size()][]);
-        System.out.println("This is x: " + x);
+
+        System.out.println(Arrays.deepToString(x).replace("], ", "]\n"));
+
+        System.out.println(deals.responseAttribute());
+        System.out.println(deals.response());
         int[] y = deals.toArray(new int[deals.size()]);
-        System.out.println("This is y: " + y);
-        LogisticRegression logisticRegression = new LogisticRegression(deals);
+        LogisticRegression logisticRegression = new LogisticRegression(x,y,0,0.00001, 1000);
+
         System.out.println(logisticRegression.loglikelihood());
+
+        double[] newData = {2.0, 7.0, 8.0, 2.0, 4.0, 7.0, 107.49, 0.0, 0.0, 1.0, 0.0, 3.0, 0.0, 1.0, 0.0, 0.0, 0.0, 75.0, 0.0, 7.0, 0.0, 1.0, 1.0, 0.0, 0.0, 86.0};
+        System.out.println(logisticRegression.predict(newData));
+
     }
 }
 
---
+++ src/main/java/com/model/regression/model.py
@@ -0,0 +1,121 @@
+# Required Python Packages
+import pandas as pd
+import numpy as np
+from plotly.graph_objs._figure import Figure
+from sklearn import linear_model
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+import plotly.graph_objs as go
+import plotly.plotly as py
+import plotly.offline
+from plotly.graph_objs import *
+
+DATASET_PATH = "/Users/stevenbenmoha/IdeaProjects/payer-model/payer-model/src/main/java/com/model/data/data.csv"
+
+def scatter_with_color_dimension_graph(feature, target, layout_labels):
+    """
+    Scatter with color dimension graph to visualize the density of the
+    Given feature with target
+    :param feature:
+    :param target:
+    :param layout_labels:
+    :return:
+    """
+    trace1 = go.Scatter(
+        y=feature,
+        mode='markers',
+        marker=dict(
+            size=16,
+            color=target,
+            colorscale='RdBu',
+            showscale=True
+        )
+    )
+
+    layout = go.Layout(
+        title=layout_labels[2],
+        xaxis=dict(title=layout_labels[0]), yaxis=dict(title=layout_labels[1]))
+    data = [trace1]
+    fig = Figure(data=data, layout=layout)
+    plot1 = plotly.offline.plot(fig)
+    py.image.save_as(fig, filename=layout_labels[1] + '_Deal.png')
+
+
+def main():
+
+    deal_data_headers = ["Channel",
+                          "Customer",
+                          "Segment/Plan",
+                          "Deal_Level_of_Control",
+                          "Therapeutic_Area",
+                          "Product",
+                          "WAC_Price",
+                          "Start_Date",
+                          "End_Date",
+                          "2018_Status_Quo_Brand_Manufacturer",
+                          "2018_Status_Quo_Tier",
+                          "2018_Status_Quo_Placement",
+                          "2018_Status_Quo_Restriction",
+                          "Deal_Brand_Manufacturer",
+                          "Deal_Tier",
+                          "Deal_Placement",
+                          "Deal_Restriction",
+                          "Deal_Rebate",
+                          "PP_Type",
+                          "PP_Threshold",
+                          "PP_Lookback_Date",
+                          "2019_Actual_Brand_Manufacturer",
+                          "2019_Actual_Tier",
+                          "2019_Actual_Placement",
+                          "2019_Actual_Restriction",
+                          "Share_Shift",
+                          "Decision"
+]
+    # Loading the Glass dataset in to Pandas dataframe
+    deal_data = pd.read_csv(DATASET_PATH, names=deal_data_headers)
+
+    X = deal_data.iloc[:,25]
+
+    # y = target values, last column of the data frame
+    y = deal_data.iloc[:,26]
+
+    print(X)
+    print(y)
+
+    # filter out the applicants that got admitted
+    admitted = deal_data.loc[y == 1]
+
+    # filter out the applicants that din't get admission
+    not_admitted = deal_data.loc[y == 0]
+
+    # plots
+
+    plt.scatter(admitted.iloc[:, 25], admitted.iloc[:, 22], s=120, label='Approved', color='blue')
+    plt.scatter(not_admitted.iloc[:, 25], not_admitted.iloc[:, 22], s=120, label='Rejected', color='red')
+    plt.xlabel('Share Shift')
+    plt.ylabel('Status')
+
+
+    plt.legend()
+    plt.show()
+
+
+    print ("Number of observations :: ", len(deal_data.index))
+    print ("Number of columns :: ", len(deal_data.columns))
+    print ("Headers :: ", deal_data.columns.values)
+
+    print ("deal_data_Share_Shift :: ", list(deal_data["Share_Shift"][:120]))
+    print("deal_data_Decision :: ", list(deal_data["Decision"][:120]))
+    # Graph Labels
+    graph_labels = ["Number of Observations", "Decision", "Share_Shift"]
+
+    scatter_with_color_dimension_graph(list(deal_data["Decision"][:120])
+                                       ,list(deal_data["Share_Shift"][:120]),
+                                       graph_labels)
+
+if __name__ == "__main__":
+    main()
+
+
